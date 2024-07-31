8e3e70045eec12d4dcdb3b7b1482010e9e4b5280
Yomguithereal <guillaumeplique@gmail.com>
2024-07-31 17:06:32 +0200

    ngrams_range

--- src/ngrams.rs
+++ src/ngrams.rs
@@ -1,5 +1,5 @@
 use std::collections::VecDeque;
-// use std::ops::RangeInclusive;
+use std::ops::RangeInclusive;
 
 pub struct NGrams<I: Iterator> {
     deque: VecDeque<I::Item>,
@@ -71,9 +71,68 @@ where
     }
 }
 
+pub struct NGramsRange<T> {
+    buffer: Vec<T>,
+    next_n: usize,
+    upper_bound: usize,
+    // TODO: genericize this type somehow?
+    current_iterator: Option<NGrams<std::vec::IntoIter<T>>>,
+}
+
+impl<T: Clone> NGramsRange<T> {
+    fn new<I>(range: RangeInclusive<usize>, inner: I) -> Self
+    where
+        I: Iterator<Item = T>,
+    {
+        if range.start() < &1 {
+            panic!("cannot compute ngrams when n < 1");
+        }
+
+        // NOTE: it must buffer the input into memory which is
+        // hardly optimal...
+        Self {
+            buffer: inner.collect(),
+            next_n: *range.start(),
+            upper_bound: *range.end(),
+            current_iterator: None,
+        }
+    }
+}
+
+impl<T: Clone> Iterator for NGramsRange<T> {
+    type Item = Vec<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match &mut self.current_iterator {
+                None => {
+                    if self.next_n > self.upper_bound {
+                        return None;
+                    }
+
+                    // TODO: not clone
+                    self.current_iterator =
+                        Some(NGrams::new(self.next_n, self.buffer.clone().into_iter()));
+
+                    self.next_n += 1;
+
+                    continue;
+                }
+                Some(inner) => match inner.next() {
+                    Some(gram) => return Some(gram),
+                    None => {
+                        self.current_iterator = None;
+                        continue;
+                    }
+                },
+            }
+        }
+    }
+}
+
 pub trait NgramsIteratorExt<I: Iterator> {
     fn ngrams(self, n: usize) -> NGrams<I>;
-    // fn ngrams_range(self, range: RangeInclusive<usize>) -> impl Iterator<Item = I::Item>;
+    fn ngrams_range(self, range: RangeInclusive<usize>) -> NGramsRange<I::Item>;
 }
 
 impl<I: Iterator> NgramsIteratorExt<I> for I
@@ -82,6 +141,9 @@ where
 {
     fn ngrams(self, n: usize) -> NGrams<I> {
         NGrams::new(n, self)
+    }
+    fn ngrams_range(self, range: RangeInclusive<usize>) -> NGramsRange<I::Item> {
+        NGramsRange::new(range, self)
     }
 }
 
@@ -139,5 +201,32 @@ mod tests {
         for (i, grams) in tests.into_iter().enumerate() {
             assert_eq!(ngrams(sentence.clone(), i + 1), grams);
         }
+    }
+
+    #[test]
+    fn test_ngrams_range() {
+        let sentence = vec!["the", "cat", "eats", "the", "mouse"];
+
+        let expected = vec![
+            vec!["the"],
+            vec!["cat"],
+            vec!["eats"],
+            vec!["the"],
+            vec!["mouse"],
+            vec!["the", "cat"],
+            vec!["cat", "eats"],
+            vec!["eats", "the"],
+            vec!["the", "mouse"],
+            vec!["the", "cat", "eats"],
+            vec!["cat", "eats", "the"],
+            vec!["eats", "the", "mouse"],
+            vec!["the", "cat", "eats", "the"],
+            vec!["cat", "eats", "the", "mouse"],
+        ];
+
+        assert_eq!(
+            sentence.into_iter().ngrams_range(1..=4).collect::<Vec<_>>(),
+            expected
+        );
     }
 }
