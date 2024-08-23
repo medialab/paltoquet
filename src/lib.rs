2b78cfd91319d48225b47ffee99437c4edca287a
Yomguithereal <guillaumeplique@gmail.com>
2024-08-23 14:03:15 +0200

    Reorganizing internals

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
-mod hashtags;
-mod ngrams;
-mod paragraphs;
+mod tokenizers;
 mod utils;
-mod words;
 
-pub use hashtags::split_hashtag;
-pub use ngrams::{ngrams_len, ngrams_range_len, NgramsIteratorExt};
-pub use paragraphs::split_paragraphs;
+pub use tokenizers::*;
 pub use utils::reduce_lengthening;
-pub use words::{WordToken, WordTokenKind, WordTokenizer, WordTokenizerBuilder, WordTokens};
