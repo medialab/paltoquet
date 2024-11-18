f5fe007fbbe6e5ba5a32845e8594209cb2d5cbe6
Yomguithereal <guillaumeplique@gmail.com>
2024-11-18 14:01:20 +0100

    Adding aerated param to paragraph tokenizer

--- src/tokenizers/paragraphs.rs
+++ src/tokenizers/paragraphs.rs
@@ -2,14 +2,20 @@ use lazy_static::lazy_static;
 use regex_automata::meta::Regex;
 
 lazy_static! {
-    static ref SPLITTER_REGEX: Regex =
+    static ref AERATED_PARAGRAPH_SPLITTER_REGEX: Regex =
         Regex::new(r#"(?:\n\r|\r\n|\r|\n)[\t\s]*(?:\n\r|\r\n|\r|\n)+"#).unwrap();
+    static ref PARAGRAPH_SPLITTER_REGEX: Regex =
+        Regex::new(r#"(?:\n\r|\r\n|\r|\n)[\t\s]*(?:\n\r|\r\n|\r|\n)*"#).unwrap();
 }
 
-pub fn split_paragraphs(text: &str) -> impl Iterator<Item = &str> {
-    SPLITTER_REGEX
-        .split(text)
-        .map(|span| &text[span.start..span.end])
+pub fn split_paragraphs(text: &str, aerated: bool) -> impl Iterator<Item = &str> {
+    let splitted = if aerated {
+        AERATED_PARAGRAPH_SPLITTER_REGEX.split(text)
+    } else {
+        PARAGRAPH_SPLITTER_REGEX.split(text)
+    };
+
+    splitted.map(|span| &text[span.start..span.end])
 }
 
 #[cfg(test)]
@@ -21,7 +27,7 @@ mod tests {
         let text = "Hello first paragraph.\n\nWhat do you do?\r\n\r\nHello Mom!\r\n\r\nAnother paragraph. Multiple sentences.\nYou see?\n\n\nHere.\n\t\nThere.\n    \nOver there!\n\nOne.\r\rTwo.\n\r  \n\rThree.";
 
         assert_eq!(
-            split_paragraphs(text).collect::<Vec<_>>(),
+            split_paragraphs(text, true).collect::<Vec<_>>(),
             vec![
                 "Hello first paragraph.",
                 "What do you do?",
@@ -33,6 +39,20 @@ mod tests {
                 "One.",
                 "Two.",
                 "Three."
+            ]
+        );
+    }
+
+    #[test]
+    fn test_aerated_paragraphs() {
+        let text = "Hello first paragraph. This is very interesting\nWhat do you do?\n\n    here";
+
+        assert_eq!(
+            split_paragraphs(text, false).collect::<Vec<_>>(),
+            vec![
+                "Hello first paragraph. This is very interesting",
+                "What do you do?",
+                "here",
             ]
         );
     }
