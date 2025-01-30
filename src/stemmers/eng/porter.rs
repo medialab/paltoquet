aaaf95cfc92800827762d87d4adae0b2ac7f56a8
Julien Pontoire <pontoirejulien@gmail.com>
2025-01-30 17:57:48 +0100

    Adding porter stemmer

---
+++ src/stemmers/eng/porter.rs
@@ -0,0 +1,274 @@
+use lazy_static::lazy_static;
+use regex_automata::meta::Regex;
+
+static VOWELS: &str = "aáàâäąåoôóøeéèëêęiíïîıuúùûüyÿæœ";
+static VOWELS_C: &str = "aáàâäąåoôóøeéèëêęiíïîıuúùûüyÿæœwx";
+
+type Steps<const N: usize> = [(&'static str, Option<&'static str>); N];
+
+lazy_static! {
+    static ref STEP1A1: Regex = Regex::new(r"^(.+?)(ss|i)es$").unwrap();
+    static ref STEP1A2: Regex = Regex::new(r"^(.+?)([^s])s$").unwrap();
+    static ref STEP1B1: Regex = Regex::new(r"^(.+?)eed$").unwrap();
+    static ref STEP1B2: Regex = Regex::new(r"(ed|ing)$").unwrap();
+    static ref STEP1B3: Regex = Regex::new(r"(at|bl|iz)$").unwrap();
+    static ref STEP1C: Regex = Regex::new(r"y$").unwrap();
+    static ref ION: Regex = Regex::new(r"(s|t)$").unwrap();
+    static ref END_E: Regex = Regex::new(r"e$").unwrap();
+
+    static ref O_RULE: Regex = Regex::new(&format!(r"(?i)[^{}][{}][^{}]$", VOWELS, VOWELS, VOWELS_C)).unwrap();
+
+    static ref LC: Regex = Regex::new(&format!("(?i)^[^{}]+", VOWELS)).unwrap();
+    static ref TV: Regex = Regex::new(&format!("(?i)[{}]+$", VOWELS)).unwrap();
+    static ref M: Regex = Regex::new(&format!("(?i)([{}]+[^{}]+)", VOWELS, VOWELS)).unwrap();
+    static ref VOWEL_IN_STEM: Regex = Regex::new(&format!("(?i)[{}]", VOWELS)).unwrap();
+}
+
+static STEP2: Steps<21> = [
+    ("ational", Some("ate")),
+    ("tional", Some("tion")),
+    ("enci", Some("ence")),
+    ("anci", Some("ance")),
+    ("izer", Some("ize")),
+    ("abli", Some("able")),
+    ("alli", Some("al")),
+    ("entli", Some("ent")),
+    ("eli", Some("e")),
+    ("ousli", Some("ous")),
+    ("ization", Some("ize")),
+    ("ation", Some("ate")),
+    ("ator", Some("ate")),
+    ("alism", Some("al")),
+    ("iveness", Some("ive")),
+    ("fulness", Some("ful")),
+    ("ousness", Some("ous")),
+    ("aliti", Some("al")),
+    ("iviti", Some("ive")),
+    ("biliti", Some("ble")),
+    ("logi", Some("log")),
+];
+
+static STEP3: Steps<7> = [
+    ("icate", Some("ic")),
+    ("ative", None),
+    ("alize", Some("al")),
+    ("iciti", Some("ic")),
+    ("ical", Some("ic")),
+    ("ful", None),
+    ("nes", None),
+];
+
+const STEP4: [&str; 18] = [
+    "al", "ance", "ence", "er", "ic", "able", "ible", "ant", "ement", "ment",
+    "ent", "ou", "ism", "ate", "iti", "ous", "ive", "ize"
+];
+
+fn double_consonant(word: &str, exceptions: Option<&str>) -> bool {
+    let chars: Vec<char> = word.chars().collect();
+
+    if chars.len() < 2 {
+        return false;
+    }
+    let last = chars[chars.len() - 1];
+    let before_last = chars[chars.len() - 2];
+    if let Some(except) = exceptions{
+        return last == before_last && !"aeiouy".contains(last) && !except.contains(last);
+    }
+    return last == before_last && !"aeiouy".contains(last)
+}
+
+fn compute_m(mut string: &str) -> usize {
+    if let Some(matched_part) = LC.find(string) {
+        let start = matched_part.end();
+        string = &string[start..];
+    }
+
+    if let Some(matched_part) = TV.find(string) {
+        let end = matched_part.start();
+        string = &string[..end];
+    }
+
+    M.find_iter(string).count()
+}
+
+
+pub fn porter_stemmer(word: &str) -> String {
+    let mut word = word.to_lowercase();
+
+    if word.len() < 3{
+        return word;
+    }
+
+    // Step 1a
+    if let Some(_) = STEP1A1.find(&word) {
+        word.truncate(word.len() - 2);
+    }
+
+    if let Some(_) = STEP1A2.find(&word) {
+        word.truncate(word.len() - 1);
+    }
+
+    // Step 1b
+    if let Some(_) = STEP1B1.find(&word) {
+        let stem = word[..word.len() - 1].to_string();
+        let m = compute_m(&stem);
+        if m > 0{
+            word.truncate(word.len() - 1);
+        }
+    }
+
+    else if let Some(matched_part) = STEP1B2.find(&word) {
+        let start = matched_part.start();    
+        let stem = &word[..start];
+        if let Some(_) = VOWEL_IN_STEM.find(&stem){
+            word = stem.to_string();
+
+            if let Some(_) = STEP1B3.find(&word) {
+                word.push_str("e")
+            }
+
+            else if double_consonant(&word, Some("lsz")) {
+                word.truncate(word.len() - 1);
+            }
+
+            else if compute_m(&word) == 1{
+                if let Some(_) = O_RULE.find(&word){
+                    word.push_str("e");
+                }
+            }
+        }
+    }   
+
+    // Step 1c
+    if let Some(_) = STEP1C.find(&word){
+        let stem = word[..word.len() - 1].to_string();        
+        if let Some(_) = VOWEL_IN_STEM.find(&stem){
+            word.truncate(word.len() - 1);
+            word.push_str("i");
+        }
+    }
+
+    // Step 2
+    for (suffix, replacement) in STEP2.iter() {
+        if word.ends_with(suffix){                
+            let stem = &word[..word.len() - suffix.len()];
+            if compute_m(&stem) > 0{
+                if let Some(value) = replacement {
+                    word = format!("{}{}", stem, value);
+                }
+                else{
+                    word = stem.to_string();
+                }
+            }
+        }
+    }
+
+    // Step 3
+    for (suffix, replacement) in STEP3.iter() {
+        if word.ends_with(suffix){                
+            let stem = &word[..word.len() - suffix.len()];
+            if compute_m(&stem) > 0{
+                if let Some(value) = replacement {
+                    word = format!("{}{}", stem, value);
+                }
+                else{
+                    word = stem.to_string();
+                }
+            }
+        }
+    }
+
+    // Step 4
+    for suffix in STEP4.iter() {
+        if word.ends_with(suffix){                
+            let stem = &word[..word.len() - suffix.len()];
+            if compute_m(&stem) > 1{
+                    word = stem.to_string();
+            }
+        }
+    }
+
+    if word.ends_with("ion"){
+        let stem = &word[..word.len() - 3];
+        if compute_m(&stem) > 1 {
+            if let Some(_) = ION.find(&stem){
+                word = stem.to_string();
+            }
+        }
+    }
+
+    // Step 5a
+    if let Some(_) = END_E.find(&word){
+        let stem = &word[..word.len() - 1];
+        let m = compute_m(&stem);
+        if m > 1{
+            word = stem.to_string();
+        } else if m == 1{
+            if O_RULE.find(&stem).is_none(){
+                word = stem.to_string();
+            }
+        }
+    }
+
+    // Step 5b
+    if double_consonant(&word, None){
+        if word.ends_with("l"){
+            let stem = &word[..word.len() - 1];
+            if compute_m(&stem) > 1{
+                word = stem.to_string();
+            }
+        }
+    }
+    word
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compute_m() {
+        assert_eq!(compute_m("tree"), 0);
+        assert_eq!(compute_m("trouble"), 1);
+        assert_eq!(compute_m("troubles"), 2);
+    }
+
+    #[test]
+    fn test_porter() {
+        let tests = [
+            ("caresses", "caress"),
+            ("cats", "cat"),
+            ("feed", "feed"),
+            ("proceed", "proce"),
+            ("plastered", "plaster"),
+            ("bled", "bled"),
+            ("motoring", "motor"),
+            ("sing", "sing"),
+            ("conflated", "conflat"),
+            ("hopping", "hop"),
+            ("failing", "fail"),
+            ("filing", "file"),
+            ("happy", "happi"),
+            ("sky", "sky"),
+            ("relational", "relat"),
+            ("rational", "ration"),
+            ("sensibiliti", "sensibl"),
+            ("triplicate", "triplic"),
+            ("formative", "form"),
+            ("revival", "reviv"),
+            ("adoption", "adopt"),
+            ("probate", "probat"),
+            ("rate", "rate"),
+            ("cease", "ceas"),
+            ("falling", "fall"),
+            ("controll", "control"),
+            ("relate", "relat"),
+            ("pirate", "pirat"),
+            ("necessitate", "necessit"),
+        ];
+
+        for (string, expected) in tests {
+            assert_eq!(porter_stemmer(string), expected);
+        }
+    }
+}
--- src/stemmers/mod.rs
+++ src/stemmers/mod.rs
@@ -1,4 +1,5 @@
 pub mod fr;
+pub mod eng;
 mod s_stemmer;
 
 pub use s_stemmer::s_stemmer;
