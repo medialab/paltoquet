0d182bbdc20671d7f465631747727b701dd27403
Yomguithereal <guillaumeplique@gmail.com>
2025-12-12 14:10:02 +0100

    Better phonogram

--- examples/phonogram.rs
+++ examples/phonogram.rs
@@ -6,28 +6,39 @@ use paltoquet::phonetics::phonogram;
 #[derive(Parser, Debug)]
 struct Args {
     /// Path to target CSV file
-    path: String,
+    #[arg(long)]
+    path: Option<String>,
+
+    /// Name to test
+    name: Vec<String>,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    let file = File::open(args.path)?;
-    let mut reader = simd_csv::Reader::from_reader(file);
-    let mut writer = simd_csv::Writer::from_writer(std::io::stdout());
 
-    let mut record = reader.byte_headers()?.clone();
-    record.push_field(b"phonogram");
+    if let Some(path) = &args.path {
+        let file = File::open(path)?;
+        let mut reader = simd_csv::Reader::from_reader(file);
+        let mut writer = simd_csv::Writer::from_writer(std::io::stdout());
 
-    writer.write_byte_record(&record)?;
-
-    while reader.read_byte_record(&mut record)? {
-        let code = phonogram(std::str::from_utf8(&record[0])?);
-        record.push_field(code.as_bytes());
+        let mut record = reader.byte_headers()?.clone();
+        record.push_field(b"phonogram");
 
         writer.write_byte_record(&record)?;
-    }
 
-    writer.flush()?;
+        while reader.read_byte_record(&mut record)? {
+            let code = phonogram(std::str::from_utf8(&record[0])?);
+            record.push_field(code.as_bytes());
+
+            writer.write_byte_record(&record)?;
+        }
+
+        writer.flush()?;
+    } else {
+        for name in args.name {
+            println!("{} => {}", &name, phonogram(&name));
+        }
+    }
 
     Ok(())
 }
--- src/phonetics/phonogram.rs
+++ src/phonetics/phonogram.rs
@@ -1,28 +1,68 @@
+use std::borrow::Cow;
+
 use lazy_static::lazy_static;
-use regex::Regex;
+use regex::{Regex, RegexBuilder};
 use unidecode::unidecode;
 
 use crate::utils::squeeze;
 
 lazy_static! {
-    static ref RULES: [(Regex, &'static str); 14] = {
+    static ref PRE_UNIDECODE_RULES: [(Regex, &'static str); 4] = {
         [
-            (r"(?:iej|ew)$", ""), // Bartholomew
-            (r"aul?[dt]$", ""), // Thibaud
-            (r"ij$", "i"), // "Dimitrij"
-            (r"ts?[cs]h|[ct]z|ich$|i[cg]$", "tʃ"), // tʃ
-            (r"pt", "t"), // Compte
-            (r"mt", "nt"), // Compte
-            (r"ph", "f"), // Stephan
-            (r"o[fvw]+$", "of"), // Smirnoff
-            (r"e[fvw]+$", "ef"), // Diaghilev
-            (r"osme", "ome"), // Cosmes
+            (r"^m['’]", "mc"),
+            (r"š|ş(k)", "sh${1}"),
+            (r"[vw]itc?z$", "vitch"),
+            (r"ç", "ss")
+        ].map(|(pattern, replacement)| (RegexBuilder::new(pattern).case_insensitive(true).build().unwrap(), replacement))
+    };
+
+    // The order of those rules IS important!
+    // TODO: factorize to reduce the number of rules
+    static ref POST_UNIDECODE_RULES: [(Regex, &'static str); 31] = {
+        [
+            (r"mic[hk]", "mik"), // Michael
+            (r"^ou([ai])", "w${1}"), // Ouattara
+            (r"kn", "n"), // Knight, Knopfler
+            (r"wr", "r"), // Wriggler, Lawrence
+            (r"ight", "it"), // Night
+            (r"^[gk]has", "as"), // Initial hard h
+            (r"blan(?:d?|cq?)$", "bl"), // Final blanc
+            (r"(t?th|m)(?:ews?|iej)$", "$1"), // Matthew, Bartolomew
+            (r"(?:ea?ux|aux|u[iy]t?s?)$", ""), // Final eux, uit
+            (r"([mp])on[dt]?$", "$1"), // Dupont, Dumont
+            (r"([ig])er$", "$1"), // Roger, Négrier
+            (r"aul?[dt]$|ot$", ""), // Thibaud, Martinot
+            (r"ij([dkn])|ij$", "i${1}"), // Stijn, Dijk, Dimitrij
+            (r"[dp]t", "t"), // Compte, Brandt
+            (r"mt", "nt"), // Comte
+            (r"p[hf]", "f"), // Stephan, Pfister
+            (r"([aeo])[fvw]+$", "${1}f"), // Smirnoff, Diagilev, Vladislav
+            (r"esti", "eti"), // Estiennes
+            (r"([ou])s([mn])", "${1}${2}"), // Cosmes, Meusnier
             (r"ast", "at"), // Gastineau
-            (r"es$", ""), // Cosmes
-            (r"th", "t"), // th
-            (r"qu?|gh?|c[aouk]", "k"), // k
+            (r"gwl", "gl"), // Gwladis
+            (r"gli", "li"), // Cuglioli
+            (r"[ao]ugh|ao?gh$", "o"), // Limbaugh
+            (r"qu?|gh?|c[aouk]|ech$|c$", "k"), // k sound
+            (r"d?(?:sch|ch|sh)([aeiouyk])|s[jz]|ts?[cs]h|cz|[cs]h$|i[cg]$", "ʃ${1}"), // ʃ sound
+            (r"j([aeiou])|gi[aou]|geo|zh", "ʒ${1}"), // ʒ sound
+            (r"([mdt])h", "$1"), // Silent h
+            (r"(?:h[aeiou]|[aeiou]h$)", "a"), // Initial or final silent h
+            (r"c[ei]|z", "s"), // s sound
+            (r"akes|c?[hk]sh?|xs", "x"), // x sound
+            (r"([elgmn])s$", "$1"), // Final s
         ]
             .map(|(pattern, replacement)| (Regex::new(pattern).unwrap(), replacement))
+    };
+
+    static ref FINAL_RULES: [(Regex, &'static str); 5] = {
+        [
+            (r"stl", "sl"),
+            (r"tl(.)", "tr${1}"),
+            (r"dʒ", "ʒ"),
+            (r"ts", "s"),
+            (r"w", "v"),
+        ].map(|(pattern, replacement)| (Regex::new(pattern).unwrap(), replacement))
     };
 }
 
@@ -30,20 +70,85 @@ fn is_vowel(c: char) -> bool {
     matches!(c, 'a' | 'e' | 'i' | 'o' | 'u' | 'y')
 }
 
+// TODO: we need the j IPA sound or we just merge it directly into ʒ
+// TODO: more aggressive version that conflates, f,b -> v, d -> t
+// TODO: joseph, richemond, lutsifer, durant durand, Campbell
+// TODO: final -ow is usually problematic
+
 pub fn phonogram(name: &str) -> String {
-    let mut code: String = unidecode(name)
+    let mut code = name.to_string();
+
+    // Applying rules, pre unidecode
+    for (pattern, replacement) in PRE_UNIDECODE_RULES.iter() {
+        if let Cow::Owned(replaced) = pattern.replace_all(&code, *replacement) {
+            code = replaced;
+        }
+    }
+
+    code = unidecode(&code)
         .to_ascii_lowercase()
         .chars()
         .filter(|c| *c >= 'a' && *c <= 'z')
         .collect();
 
-    // Applying rules
-    for (pattern, replacement) in RULES.iter() {
-        code = pattern.replace(&code, *replacement).into_owned();
+    // Short name exceptions
+    match code.as_str() {
+        "li" | "lee" => return "li".to_string(),
+        "ali" | "eli" => return "ali".to_string(),
+        "lea" | "leah" => return "lea".to_string(),
+        "ida" | "ada" => return "ada".to_string(),
+        _ => (),
+    };
+
+    // Applying rules, post unidecode
+    for (pattern, replacement) in POST_UNIDECODE_RULES.iter() {
+        if let Cow::Owned(replaced) = pattern.replace_all(&code, *replacement) {
+            code = replaced;
+        }
     }
 
-    // Dropping vowels
+    // Squeezing, then dropping vowels
     code = squeeze(&code).chars().filter(|c| !is_vowel(*c)).collect();
 
+    // Applying final rules
+    for (pattern, replacement) in FINAL_RULES.iter() {
+        if let Cow::Owned(replaced) = pattern.replace_all(&code, *replacement) {
+            code = replaced;
+        }
+    }
+
     code
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_phonogram() {
+        let tests = [
+            ("Comte", "knt"),
+            ("Compte", "knt"),
+            ("Compte", "knt"),
+            ("Thibault", "tb"),
+            ("Martinot", "mrtn"),
+            ("Gold", "kld"),
+            ("Ghosn", "kn"),
+            ("Ghassan", "sn"),
+            ("Ghana", "kn"),
+            ("Little", "ltl"),
+            ("Catherine", "ktrn"),
+            ("Caitlyn", "ktrn"),
+            ("Leblanc", "lbl"),
+            ("Knopfler", "nflr"),
+            ("Knight", "nt"),
+            ("Dupond", "dp"),
+            ("Estiennes", "tn"),
+            ("Dimitrij", "dmtr"),
+        ];
+
+        for (name, code) in tests {
+            assert_eq!(phonogram(name), code, "{} => {}", name, code);
+        }
+    }
 }
